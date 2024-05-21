9b4cbf4476e3309103b444418bf0b38c4e607175
Abraham <abrahamnhier97@gmail.com>
2024-05-21 09:58:02 +0300

    production

--- utils/jwt.js
+++ utils/jwt.js
@@ -20,7 +20,7 @@ const attachCookiesToResponse = ({ res, user }) => {
     res.cookie('token', token, {
       httpOnly: true,
       expires: new Date(Date.now() + oneDay),
-      secure: process.env.NODE_ENV === 'production' ? true : false,
+      secure: process.env.NODE_ENV === 'production',
       signed: true,
     });
     console.log('Token attached to response:', token);
