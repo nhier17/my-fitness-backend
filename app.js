5934c0fc756bdfd5721f92f57ca8e7c89e4cefa2
Abraham <abrahamnhier97@gmail.com>
2024-05-21 10:15:17 +0300

    cors configuration

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 //cors options
 const corsOptions = {   
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
     allowHeaders: ['Content-Type', 'Authorization'],
--- utils/jwt.js
+++ utils/jwt.js
@@ -21,10 +21,10 @@ const attachCookiesToResponse = ({ res, user }) => {
       httpOnly: true,
       expires: new Date(Date.now() + oneDay),
       secure: process.env.NODE_ENV === 'production',
+      sameSite: 'none',
       signed: true,
     });
-    console.log('Token attached to response:', token);
-  };
+      };
 
   module.exports = {
     createJWT,
