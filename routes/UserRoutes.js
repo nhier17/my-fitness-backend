fc1fbaa8f537fed40e29c7a774d1739254ef0ff5
Abraham <abrahamnhier97@gmail.com>
2024-05-27 15:17:10 +0300

    authenticate user

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -16,6 +16,6 @@ router.route('/').get(getAllUsers);
 router.route('/current').get(authenticateUser, showCurrentUser);
 router.route('/:id').get(authenticateUser, getSingleUser);
 router.route('/update-user').patch(updateUser);
-router.route('/update-password').patch(updatePassword);
+router.route('/update-password').patch(authenticateUser, updatePassword);
 
 module.exports = router;
